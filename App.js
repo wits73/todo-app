b9fd0a3ef946f14f1aa92637cbd532c1890ec1e0
Tae Kim <tkim25@kennesaw.edu>
2018-09-07 09:49:06 -0400

    basic structure

---
+++ App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { StyleSheet, Text, View, StatusBar, Dimensions, TextInput, Platform } from 'react-native';
+
+const { height, width } = Dimensions.get("window");
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>To Do App!</Text>
+        <View style={styles.card}>
+          <TextInput placeholder={"New To Do"}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F23657',
+    alignItems: 'center',
+  },
+  title: {
+    color: "white",
+    fontSize: 30,
+    marginTop: 50,
+    fontWeight: "200",
+    marginBottom: 30
+  },
+  card: {
+    backgroundColor: "white",
+    flex: 1,
+    width: width - 25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgb(50, 50, 50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 3
+      }
+    }),
+  },
+});
